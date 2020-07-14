2270a0bd89ba35cadae620a626f3c294a2cea5a4
IvanLuLyf <835498692@qq.com>
2020-07-14 21:23:24 +0800

    add router test

---
+++ app/controller/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ac_index: function () {
+        return 'Hello World';
+    },
+    ac_test: function () {
+        return 'Test';
+    },
+};
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ var http = require('http');
 var PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
-    console.log(request);
+    console.log(request.url);
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('BunnyJS');
+    var url_array = request.url.split('/');
+    if (!url_array[1]) url_array[1] = 'index';
+    try {
+        var controller = require('../app/controller/' + url_array[1]);
+        var action = url_array[2] || 'index';
+        response.end(controller['ac_' + action]());
+    }catch (e) {
+        
+    }
 }).listen(PORT);
 
 console.log('BunnyJS\nServing HTTP on port ' + PORT + '...\nRunning on http://127.0.0.1:' + PORT + '/ (Press CTRL+C to quit)');
+
